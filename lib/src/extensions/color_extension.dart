9726b410d33bda277820a0246fe04e239045edb4
Kobasic <83089068+Kobasic@users.noreply.github.com>
2025-02-03 14:25:22 +0100

    Update color_extension.dart

--- lib/src/extensions/color_extension.dart
+++ lib/src/extensions/color_extension.dart
@@ -1,24 +1,19 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
 extension ColorExtension on Color {
-  /// Convert the color to a darken color based on the [percent]
-  Color darken([int percent = 40]) {
-    assert(1 <= percent && percent <= 100);
-    final value = 1 - percent / 100;
-    return Color.fromARGB(
-      _floatToInt8(a),
-      (_floatToInt8(r) * value).round(),
-      (_floatToInt8(g) * value).round(),
-      (_floatToInt8(b) * value).round(),
-    );
-  }
+  // Vraća alpha vrijednost između 0.0 i 1.0
+  double get aValue => alpha / 255.0;
+  int get rValue => red;
+  int get gValue => green;
+  int get bValue => blue;
 
-  // Int color components were deprecated in Flutter 3.27.0.
-  // This method is used to convert the new double color components to the
-  // old int color components.
-  //
-  // Taken from the Color class.
-  int _floatToInt8(double x) {
-    return (x * 255.0).round() & 0xff;
+  // Zamijenite withValues metodom koja radi slično
+  Color withValues({double? alpha}) {
+    return Color.fromARGB(
+      alpha != null ? (alpha * 255).round() : this.alpha,
+      red,
+      green,
+      blue,
+    );
   }
 }
